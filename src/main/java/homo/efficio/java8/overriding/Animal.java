842bdfd13510c92abf8f5bc30c56d070b791dc6d
HomoEfficio <homo.efficio@gmail.com>
2017-01-07 00:22:14 +0900

    오버라이딩 실험 코드 추가

---
+++ src/main/java/homo/efficio/java8/overriding/Animal.java
@@ -0,0 +1,12 @@
+package homo.efficio.java8.overriding;
+
+/**
+ * @author homo.efficio@gmail.com
+ *         create on 2016-12-05.
+ */
+public class Animal {
+
+    public void move() {
+        System.out.println("Animal moves");
+    }
+}
