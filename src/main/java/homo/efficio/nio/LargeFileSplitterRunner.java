7ec3144349cf7b7aa8ab9d2ef8981139c7b47fee
Homo Efficio <homo.efficio@gmail.com>
2019-02-27 00:37:03 +0900

    벤치마크 코드 주석 해제

--- src/main/java/homo/efficio/nio/LargeFileSplitterRunner.java
+++ src/main/java/homo/efficio/nio/LargeFileSplitterRunner.java
@@ -1,8 +1,11 @@
 package homo.efficio.nio;
 
+import java.io.File;
 import java.io.IOException;
 import java.nio.ByteBuffer;
+import java.nio.charset.StandardCharsets;
 
+import static homo.efficio.nio.LargeFileSplitterIO.splitFileIntoDir;
 import static homo.efficio.nio.LargeFileSplitterIO.splitFileIntoDirChannel;
 
 /**
@@ -22,45 +25,45 @@ public class LargeFileSplitterRunner {
 //        }
 //        System.out.println("]");
 
-//        Thread.sleep(8000L);
+        Thread.sleep(8000L);
 
         runSplitterChannel(
-//                "./out/production/resources/file-split/test-data.csv",
-                "./src/main/resources/file-split/test-short",
+                "./out/production/resources/file-split/test-data.csv",
+//                "./src/main/resources/file-split/test-short",
                 "./src/main/resources/file-split/splitted",
                 "1%04d",
-//                ByteBuffer.allocateDirect(10 * 1024 * 1024)
-                ByteBuffer.allocateDirect(65)
+                ByteBuffer.allocateDirect(100 * 1024 * 1024)
+//                ByteBuffer.allocateDirect(65)
         );
 
 
-//        Thread.sleep(5000L);
-//
-//
-//        runSplitterChannel(
-//                "./out/production/resources/file-split/test-data.csv",
-//                "./out/production/resources/file-split/splitted",
-//                "2%04d",
-//                ByteBuffer.allocate(10 * 1024 * 1024)
-//        );
-//
-//
-//        Thread.sleep(5000L);
-//
-//
-//        System.out.println("==============");
-//        System.out.println("파일 나누기 시작");
-//        long startTime = System.nanoTime();
-//        final File srcFile = new File("./out/production/resources/file-split/test-data.csv");
-//
-//        splitFileIntoDir(srcFile, 10, new File("./out/production/resources/file-split/splitted"));
-//
-//        long endTime = System.nanoTime();
-//        System.out.println("파일 나누기 완료");
-//        System.out.println("소요 시간(초): " + (endTime - startTime));
-//
-//
-//        Thread.sleep(8000L);
+        Thread.sleep(5000L);
+
+
+        runSplitterChannel(
+                "./out/production/resources/file-split/test-data.csv",
+                "./out/production/resources/file-split/splitted",
+                "2%04d",
+                ByteBuffer.allocate(100 * 1024 * 1024)
+        );
+
+
+        Thread.sleep(5000L);
+
+
+        System.out.println("==============");
+        System.out.println("파일 나누기 시작");
+        long startTime = System.nanoTime();
+        final File srcFile = new File("./out/production/resources/file-split/test-data.csv");
+
+        splitFileIntoDir(srcFile, 100, new File("./out/production/resources/file-split/splitted"));
+
+        long endTime = System.nanoTime();
+        System.out.println("파일 나누기 완료");
+        System.out.println("소요 시간(초): " + (endTime - startTime));
+
+
+        Thread.sleep(8000L);
     }
 
     private static void runSplitterChannel(String srcFilePath,
